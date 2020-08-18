ba8bbfe8c686a870e550c301f2d9d6a771c9c64e
joanna Kang <jfrences11@gmail.com>
2020-08-18 19:59:37 +0100

    json update

---
+++ main.js
@@ -0,0 +1,61 @@
+//fetch info from the json
+function loadItems() {
+  return fetch("Data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.personalinfo);
+}
+
+function displayItems(personalinfo) {
+  const container = document.querySelector(".personinfo");
+  container.innerHTML = personalinfo
+    .map((info) => createHTMLString(info))
+    .join("");
+  console.log(container);
+}
+
+function createHTMLString(personalinfo) {
+  return `
+  <div class="personinfo">
+  <div class="name">
+    <img src="${personalinfo.imgsrc}" alt="" /> ${personalinfo.firstname} ${personalinfo.lastname} 
+  </div>
+
+  <div class="detailinfo">
+    <ul>
+      Mobile: ${personalinfo.mobile}
+    </ul>
+    <ul>
+      Email: ${personalinfo.email}
+    </ul>
+    <ul>
+      Address: ${personalinfo.address}
+    </ul>
+    <div class="actionbuttons">
+      <button class="icon">
+        <i class="fab fa-facebook-messenger"></i>
+      </button>
+      <button class="icon">
+        <i class="fas fa-phone-alt"></i>
+      </button>
+      <button class="icon">
+        <i class="fas fa-envelope"></i>
+      </button>
+      <button class="text-edit">
+        EDIT
+      </button>
+      <button class="text-delete">
+        DELETE
+      </button>
+    </div>
+  </div>
+</div>
+  `;
+}
+
+//get info from json
+loadItems()
+  .then((personalinfo) => {
+    console.log(personalinfo);
+    displayItems(personalinfo);
+  })
+  .catch(console.log);
