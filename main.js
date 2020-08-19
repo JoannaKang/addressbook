2c191be16a9153c3454d6bf88df3160d6214d9ae
joanna Kang <jfrences11@gmail.com>
2020-08-19 02:23:37 +0100

    search filter

--- main.js
+++ main.js
@@ -1,26 +1,70 @@
-//fetch info from the json
-function loadItems() {
-  return fetch("Data/data.json")
-    .then((response) => response.json())
-    .then((json) => json.personalinfo);
-}
+function loadPage() {
+  //fetch info from the json
 
-function displayItems(personalinfo) {
-  const container = document.querySelector(".personinfo");
-  container.innerHTML = personalinfo
-    .map((info) => createHTMLString(info))
-    .join("");
+  function loadItems() {
+    return fetch("Data/data.json")
+      .then((response) => response.json())
+      .then((json) => json.personalinfo);
+  }
 
-  console.log(container);
-}
+  //get info from json
+  loadItems()
+    .then((personalinfo) => {
+      displayItems(personalinfo);
+    })
+    .catch(console.log);
 
-function createHTMLString(personalinfo) {
-  return `
+  let searchstring = document.getElementById("myInput").value;
+  console.log(searchstring);
+
+  function displayItems(personalinfo) {
+    if (searchstring === "") {
+      const container = document.querySelector(".personinfo");
+      let htmlString = "";
+      let alpaphet = "abcdefghijklmnopqrstuvwxyz";
+      alpaphet = alpaphet.toUpperCase();
+
+      for (j = 0; j < alpaphet.length; j++) {
+        htmlString += `<div class="contactlist">${alpaphet[j]}</div>`;
+        for (i = 0; i < personalinfo.length; i++) {
+          if (personalinfo[i].firstname[0] === alpaphet[j]) {
+            console.log(personalinfo[i].firstname);
+            htmlString += createHTMLString(personalinfo[i]);
+          }
+        }
+      }
+      container.innerHTML = htmlString;
+    } else {
+      htmlString = "";
+      const container = document.querySelector(".personinfo");
+      container.innerHTML = htmlString;
+      for (i = 0; i < personalinfo.length; i++) {
+        lowercase_search = searchstring.toLowerCase();
+        lowercase_lastname = personalinfo[i].lastname
+          .substring(0, searchstring.length)
+          .toLowerCase();
+        lowercase_firstname = personalinfo[i].firstname
+          .substring(0, searchstring.length)
+          .toLowerCase();
+        if (
+          lowercase_search === lowercase_lastname ||
+          lowercase_search === lowercase_firstname
+        ) {
+          htmlString += createHTMLString(personalinfo[i]);
+          container.innerHTML = htmlString;
+        }
+      }
+    }
+  }
+
+  function createHTMLString(personalinfo) {
+    return `
   <div class="name">
     <img src="${personalinfo.imgsrc}" alt="" /> ${personalinfo.firstname} ${personalinfo.lastname} 
   </div>
 
   <div class="detailinfo">
+
     <ul>
       Mobile: ${personalinfo.mobile}
     </ul>
@@ -50,13 +94,8 @@ function createHTMLString(personalinfo) {
     </div>
 
   </div>
-
   `;
+  }
 }
 
-//get info from json
-loadItems()
-  .then((personalinfo) => {
-    displayItems(personalinfo);
-  })
-  .catch(console.log);
+loadPage();
