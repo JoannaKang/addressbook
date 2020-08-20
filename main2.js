9b663aacbb783b9299472e9ca4725becd50d4cf4
joanna Kang <jfrences11@gmail.com>
2020-08-20 19:04:40 +0100

    modify start

--- main2.js
+++ main2.js
@@ -1,3 +1,4 @@
+//0.Get Data
 //set initial variable to save json data
 let initialData = new Array();
 
@@ -10,6 +11,7 @@ function getData() {
     });
 }
 
+//1.Dispay Data
 function displayPage() {
   //Insert HTML tag to ".personinfo" div tag
   let htmlString = "";
@@ -17,7 +19,6 @@ function displayPage() {
 
   //get search strings from searchbox
   let searchstring = document.getElementById("myInput").value;
-  console.log(searchstring);
 
   //create name group by first letter of first name
   let alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -84,7 +85,7 @@ function createHTMLstring(initialData) {
         <button class="icon">
           <i class="fas fa-envelope"></i>
         </button>
-        <button class="text-edit">
+        <button class="text-edit" onclick="openEditBox('${initialData.dataindex}');">
           EDIT
         </button>
         <button class="text-delete" onclick="openDeleteBox('${initialData.firstname} ${initialData.lastname}');">
@@ -95,19 +96,21 @@ function createHTMLstring(initialData) {
     </div>
     `;
 }
-let delete_name;
+
+//3. Delete data
+let deleted_data;
 function openDeleteBox(fullname) {
-  delete_name = fullname;
+  deleted_data = fullname;
   document.querySelector(".delete-modal").style.display = "flex";
   console.log("open delete box");
 }
 
 function deleteName() {
   console.log("delete name");
-  console.log(delete_name);
+  console.log(deleted_data);
   for (i = 0; i < initialData.length; i++) {
     if (
-      delete_name ===
+      deleted_data ===
       initialData[i].firstname + " " + initialData[i].lastname
     ) {
       console.log("deleting value " + i);
@@ -119,6 +122,7 @@ function deleteName() {
   document.querySelector(".delete-modal").style.display = "none";
 }
 
+//3-1. Delete Box Control
 //close delete box
 document
   .querySelector(".deletepopup-close")
@@ -126,11 +130,72 @@ document
     document.querySelector(".delete-modal").style.display = "none";
   });
 
-//open add info popup
-document.getElementById("addbutton").addEventListener("click", function () {
-  document.querySelector(".bg-modal").style.display = "flex";
+document.querySelector(".discard").addEventListener("click", function () {
+  document.querySelector(".delete-modal").style.display = "none";
 });
 
+//open delete check popup
+
+getData();
+displayPage();
+
+//4. Addinfo
+let newinfo = {};
+let new_firstname;
+let new_lastname;
+let new_mobile;
+let new_email;
+let new_address;
+let new_profile;
+
+function addNewInfo() {
+  newinfo.dataindex = `${initialData.length}`;
+  imglist = [
+    "../icon/1752632-pokemon/png/032-avatar.png",
+    "../icon/1752632-pokemon/png/033-avatar.png",
+    "../icon/1752632-pokemon/png/034-avatar.png",
+    "../icon/1752632-pokemon/png/036-avatar.png",
+    "../icon/1752632-pokemon/png/037-avatar.png",
+    "../icon/1752632-pokemon/png/039-avatar.png",
+    "../icon/1752632-pokemon/png/041-avatar.png",
+  ];
+  let rand = Math.floor(Math.random() * imglist.length);
+  newinfo.imgsrc = imglist[rand];
+  console.log(newinfo);
+  initialData.push(newinfo);
+  console.log(initialData);
+  document.addEventListener("click", function () {
+    document.querySelector(".bg-modal").style.display = "none";
+  });
+  displayPage();
+}
+
+function getFirstName() {
+  new_firstname = document.getElementById("newfirstname").value;
+  newinfo.firstname = new_firstname;
+}
+
+function getLastName() {
+  new_lastname = document.getElementById("newlastname").value;
+  newinfo.lastname = new_lastname;
+}
+
+function getMobile() {
+  new_mobile = document.getElementById("newmobile").value;
+  newinfo.mobile = new_mobile;
+}
+
+function getEmail() {
+  new_email = document.getElementById("newemail").value;
+  newinfo.email = new_email;
+}
+
+function getAddress() {
+  new_address = document.getElementById("newaddress").value;
+  newinfo.address = new_address;
+}
+
+//4-1. Add Box Control
 //close info popup
 document
   .querySelector(".addpopup-close")
@@ -138,7 +203,33 @@ document
     document.querySelector(".bg-modal").style.display = "none";
   });
 
-//open delete check popup
+//open add info popup
+document.getElementById("addbutton").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "flex";
+});
 
-getData();
-displayPage();
+//5. Edit
+let modifydata;
+let modifydataindex;
+
+function openEditBox(dataindex) {
+  modifydataindex = dataindex;
+  console.log("editclick");
+  console.log(modifydataindex);
+  console.log(initialData[modifydataindex].firstname);
+
+  // function createFirstname(modifydataindex) {
+  //   currentFisrtname = document.createTextnode(
+  //     `${initialData[modifydataindex].firstname}`
+  //   );
+  //   document.body.appendChild(newfirstname);
+  //   console.log("currentname");
+  // }
+
+  document.querySelector(".bg-modal").style.display = "flex";
+}
+
+function editData(dataindex) {
+  modifydataindex = dataindex;
+  onsole.log(modifydataindex);
+}
